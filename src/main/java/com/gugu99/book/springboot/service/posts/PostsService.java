3a588d79b3b2caab818e0a3e5722b4462eee1167
GuGU <wnduddl3409@naver.com>
2019-12-19 20:10:28 +0900

    ADD Posts DELETE

--- src/main/java/com/gugu99/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/gugu99/book/springboot/service/posts/PostsService.java
@@ -2,10 +2,7 @@ package com.gugu99.book.springboot.service.posts;
 
 import com.gugu99.book.springboot.domain.posts.Posts;
 import com.gugu99.book.springboot.domain.posts.PostsRepository;
-import com.gugu99.book.springboot.web.dto.PostsListResponseDto;
-import com.gugu99.book.springboot.web.dto.PostsResponseDto;
-import com.gugu99.book.springboot.web.dto.PostsSaveRequestDto;
-import com.gugu99.book.springboot.web.dto.PostsUpdateRequestDto;
+import com.gugu99.book.springboot.web.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,11 +23,19 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("해당 게시글 없습니다. id=" + id));
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById (Long id) {
