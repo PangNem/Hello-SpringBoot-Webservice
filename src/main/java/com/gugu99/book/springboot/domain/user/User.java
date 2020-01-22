27f33aeaf69d504b2e16219c9ccf909d111f8758
GuGU <wnduddl3409@naver.com>
2020-01-22 21:17:21 +0900

    [Create] Role, UserRepository to User Domain

--- src/main/java/com/gugu99/book/springboot/domain/user/User.java
+++ src/main/java/com/gugu99/book/springboot/domain/user/User.java
@@ -1,6 +1,7 @@
 package com.gugu99.book.springboot.domain.user;
 
 import com.gugu99.book.springboot.domain.posts.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,4 +29,25 @@ public class User extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+
 }
