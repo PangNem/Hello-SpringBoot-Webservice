d3a63d5dee7546e408454c5b2caac6064509f807
GuGU <wnduddl3409@naver.com>
2020-01-23 22:36:27 +0900

    [Create] OAuthAttributes, SessionUser

--- src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
+++ src/main/java/com/gugu99/book/springboot/web/dto/indexController.java
@@ -1,36 +1,29 @@
 package com.gugu99.book.springboot.web.dto;
 
+import com.gugu99.book.springboot.config.auth.dto.*;
 import com.gugu99.book.springboot.service.posts.PostsService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class indexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
-        return "index";
-    } // 가져온 결과를 posts로 index에 전달한다.
-
-    @GetMapping("/posts/save")
-    public String postsSave() {
-        return "posts-save";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index"; // 가져온 결과를 posts로 index에 전달한다.
     }
 
 }
