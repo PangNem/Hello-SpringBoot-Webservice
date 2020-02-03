f854927506403f41f8b76ba187afad75a6693b61
PangNem <sprnd645@gmail.com>
2020-02-03 22:45:09 +0900

    [Update] missing annotation in LoginUserArgumentResolver

--- src/main/java/com/gugu99/book/springboot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/gugu99/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -1,7 +1,9 @@
 package com.gugu99.book.springboot.config.auth;
 
 import com.gugu99.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -9,6 +11,8 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpSession;
 
+@RequiredArgsConstructor
+@Component
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final HttpSession httpSession;
@@ -17,7 +21,6 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     public boolean supportsParameter(MethodParameter parameter) {
         boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
         boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
-
         return isLoginUserAnnotation && isUserClass;
     }
 
